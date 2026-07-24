26f0890828906e6bc4c347f0ab3aa465fa30bc91
chaehyunlim <chaehyun010104@gmail.com>
2026-07-24 14:49:27 +0900

    feat : bag 게임 수정

--- 0723-0724-HTML, CSS, JavaScript— Day1/실습ppt과제/script/bag.js
+++ 0723-0724-HTML, CSS, JavaScript— Day1/실습ppt과제/script/bag.js
@@ -2,7 +2,10 @@ function showMyBag() {
   var myBag = [
     { name: "여권", count: 1 },
     { name: "스마트폰", count: 2 },
-    { name: "지갑", count: 1 }
+    { name: "지갑", count: 1 },
+    { name: "텀블러", count: 1 },
+    { name: "옷", count: 7 },
+    { name: "신발", count: 2 }
   ];
 
   var result = "[내 가방 속 물품 목록]\n--------------------\n";
