642c6b1392a6a936af93b346acdc0d2ebc11a678
chaehyunlim <chaehyun010104@gmail.com>
2026-08-10 17:00:30 +0900

    feat : java day1 실습

---
+++ 0810-0814-ava, SpringBoot, Rest API 구현_Day1,2,3,4,5/skala-java-springboot/mycode/hello-world/src/Skala.java
@@ -0,0 +1,7 @@
+public class Skala {
+    public static void main(String[] args) throws Exception {
+        System.out.print("스칼라에 오신것을 ");
+        Thread.sleep(1000);
+        System.out.println("환영합니다");
+    }
+}
