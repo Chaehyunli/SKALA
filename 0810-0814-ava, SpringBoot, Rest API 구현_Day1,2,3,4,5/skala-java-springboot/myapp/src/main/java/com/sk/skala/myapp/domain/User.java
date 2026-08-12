4cd309da0e3dffa37f6419de915f3617a2bbcd4a
chaehyunlim <chaehyun010104@gmail.com>
2026-08-12 16:43:55 +0900

    feat : spring boot day3 실습2 추가

--- 0810-0814-ava, SpringBoot, Rest API 구현_Day1,2,3,4,5/skala-java-springboot/myapp/src/main/java/com/sk/skala/myapp/domain/User.java
+++ 0810-0814-ava, SpringBoot, Rest API 구현_Day1,2,3,4,5/skala-java-springboot/myapp/src/main/java/com/sk/skala/myapp/domain/User.java
@@ -1,40 +1,23 @@
 package com.sk.skala.myapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
-
-    public User() {
-    }
-
-    public User(long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
